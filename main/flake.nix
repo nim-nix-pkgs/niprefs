a12b7165f41223aeb344eab5843fd70abf071f06
hugosenari <hugosenari@gmail.com>
2022-03-22 01:20:21 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-niprefs-main.flake = false;
+  inputs.src-niprefs-main.ref   = "refs/heads/main";
   inputs.src-niprefs-main.owner = "Patitotective";
-  inputs.src-niprefs-main.ref   = "main";
   inputs.src-niprefs-main.repo  = "niprefs";
   inputs.src-niprefs-main.type  = "github";
   
