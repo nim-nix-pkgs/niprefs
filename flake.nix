0213012e848fca084f89f9c9bee15fa8a997ec09
hugosenari <hugosenari@gmail.com>
2022-05-17 02:41:28 -0300

    chore: re index niprefs

--- flake.nix
+++ flake.nix
@@ -87,6 +87,22 @@
   inputs."niprefs-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
   inputs."niprefs-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
+  inputs."niprefs-0_2_1".dir   = "0_2_1";
+  inputs."niprefs-0_2_1".owner = "nim-nix-pkgs";
+  inputs."niprefs-0_2_1".ref   = "master";
+  inputs."niprefs-0_2_1".repo  = "niprefs";
+  inputs."niprefs-0_2_1".type  = "github";
+  inputs."niprefs-0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niprefs-0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."niprefs-0_2_2".dir   = "0_2_2";
+  inputs."niprefs-0_2_2".owner = "nim-nix-pkgs";
+  inputs."niprefs-0_2_2".ref   = "master";
+  inputs."niprefs-0_2_2".repo  = "niprefs";
+  inputs."niprefs-0_2_2".type  = "github";
+  inputs."niprefs-0_2_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."niprefs-0_2_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
